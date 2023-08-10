c24c5e46b59b9f4d505d67274baa1a4fe253b66c
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-08-10 16:59:02 +0900

    init: [김재경] Java Spring 초기설정 [PCB-1]

---
+++ src/test/java/com/example/teamproject/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
