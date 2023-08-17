d11cadf920bd75877ec886b3ac354361e3947063
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-08-17 09:23:20 +0900

    fix: [김재경] 클래스 패스 추가 [PMB]

--- src/main/java/com/example/teamproject/config/CorsConfig.java
+++ src/main/java/com/example/teamproject/config/CorsConfig.java
@@ -4,12 +4,14 @@ package com.example.teamproject.config;
 import com.example.teamproject.utility.PropertyUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@PropertySource(value = "classpath:application.properties")
 public class CorsConfig implements WebMvcConfigurer {
 
     private final PropertyUtil propertyUtil;
