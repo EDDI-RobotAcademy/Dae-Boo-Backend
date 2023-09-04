fbbd3bace536260762205d9ba8ccc322d94b105f
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-04 11:32:04 +0900

    update: [김재경] 관리자의 카드 리스트를 요청은 모든 리스트를 리턴합니다. [PMB-13]

--- src/main/java/com/example/teamproject/card/service/CardService.java
+++ src/main/java/com/example/teamproject/card/service/CardService.java
@@ -1,13 +1,12 @@
 package com.example.teamproject.card.service;
 
-import com.example.teamproject.card.entity.CardDetail;
 
-
+import com.example.teamproject.card.entity.Card;
 
 import java.util.List;
 
 
 public interface CardService {
 
-    List<CardDetail> getcard();
+    List<Card> getActivateCard();
 }
