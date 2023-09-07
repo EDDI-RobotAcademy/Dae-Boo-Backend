a0a950e772a8f1fc92d436023a6eff412287f4a7
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-09-07 09:11:19 +0900

    feat: [김재경] UserService의 서비스가 많아 User와 Oauth로 분리합니다 [PMB-37]

---
+++ src/main/java/com/example/teamproject/user/service/OauthService.java
@@ -0,0 +1,19 @@
+package com.example.teamproject.user.service;
+
+import com.example.teamproject.user.dto.KakaoOAuthToken;
+import com.example.teamproject.user.dto.NaverOAuthToken;
+import com.example.teamproject.user.entity.User;
+import org.springframework.http.HttpHeaders;
+
+public interface OauthService {
+
+    String getAuthorizeCode();
+
+    NaverOAuthToken generateAccessToken(String code);
+
+    User getNaverUserInfo(String accessToken, HttpHeaders headers);
+
+    String getKakaoAuthorizeCode();
+
+    KakaoOAuthToken kakaoCallback(String code);
+}
