9862c6c60a69d415f9332d8e9020eccf90279d5a
zisoo <zisoo_3@naver.com>
2023-09-18 16:26:28 +0900

    update: [최지수] User 엔티티 수정 [PMF-7]

--- src/main/java/com/example/teamproject/user/entity/User.java
+++ src/main/java/com/example/teamproject/user/entity/User.java
@@ -20,7 +20,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String nickname;
-    private String profile_image;
     private String age;
     private String gender;
     private String mobile;
@@ -28,25 +27,18 @@ public class User {
     private UserInterest interest1;
     private UserInterest interest2;
 
-    @Setter
-    @JsonBackReference
-    @OneToMany(mappedBy = "user")
-    private List<Comment> comments;
-
     @Column(name = "activate", columnDefinition = "boolean default true")
     private Boolean activate = true;
     private UserRole role = UserRole.NORMAL;
 
-    public User(String nickname, String email, String profileImage, String gender, String ageRange) {
+    public User(String nickname, String email, String gender, String ageRange) {
         this.nickname = nickname;
         this.email = email;
-        this.profile_image = profileImage;
         this.gender = gender;
         this.age = ageRange;
     }
 
     public void modify(String nickname, String mobile, String email, UserInterest interest1, UserInterest interest2) {
-//        public void modify(String nickname, String mobile, String email, UserInterest interest1) {
         this.nickname = nickname;
         this.mobile = mobile;
         this.email = email;
@@ -59,7 +51,6 @@ public class User {
     }
 
     public void setInterests(UserInterest interest1, UserInterest interest2) {
-//        public void setInterests(UserInterest interest1) {
         this.interest1 = interest1;
         this.interest2 = interest2;
     }
