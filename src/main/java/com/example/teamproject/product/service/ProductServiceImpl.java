77a223060fd7971c46cedecd5c73f7f7a843daa0
dusdn943 <128335427+dusdn943@users.noreply.github.com>
2023-09-19 14:14:39 +0900

    feat: [최연우] 카드 shop에서 상품 상세페이지를 조회합니다 [PMB-52]

--- src/main/java/com/example/teamproject/product/service/ProductServiceImpl.java
+++ src/main/java/com/example/teamproject/product/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.teamproject.product.service;
 
+import com.example.teamproject.product.dto.ProductDetailResponse;
 import com.example.teamproject.product.dto.ProductResponse;
 import com.example.teamproject.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +19,12 @@ public class ProductServiceImpl implements ProductService {
         return productRepository.findAllByIsDeletedFalse().stream()
                 .map(ProductResponse::from)
                 .toList();
+    }
+
+    @Override
+    public ProductDetailResponse retrieve(long productId) {
+        return productRepository.findByProductIdAndIsDeletedFalse(productId)
+                .map(ProductDetailResponse::from)
+                .orElseThrow(() -> new RuntimeException("해당 상품에 대한 정보를 찾을 수 없습니다."));
     }
 }
