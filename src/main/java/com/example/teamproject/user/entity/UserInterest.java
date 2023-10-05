2c59f0417654cb9f0870c4bf2919df13d0116cde
zisoo <zisoo_3@naver.com>
2023-10-05 17:59:09 +0900

    update: [최지수] 관심사 실제 값 입력 후 변경 [PMB-17]

--- src/main/java/com/example/teamproject/user/entity/UserInterest.java
+++ src/main/java/com/example/teamproject/user/entity/UserInterest.java
@@ -1,19 +1,19 @@
 package com.example.teamproject.user.entity;
 
 public enum UserInterest {
-    INTEREST1("관심사1"),
-    INTEREST2("관심~사2"),
-    INTEREST3("관심사3"),
-    INTEREST4("관심사4");
-
-//    private String interest;
-private String interest1;
-
-
-//    UserInterest(String interest) {
-//        this.interest = interest;
-//    }
-UserInterest(String interest1) {
-    this.interest1 = interest1;
-}
+//    INTEREST1("관리비"),
+//    INTEREST2("관심~사2"),
+//    INTEREST3("관심사3"),
+//    INTEREST4("관심사4");
+//
+////    private String interest;
+//private String interest1;
+//
+//
+////    UserInterest(String interest) {
+////        this.interest = interest;
+////    }
+//UserInterest(String interest1) {
+//    this.interest1 = interest1;
+//}
 }
